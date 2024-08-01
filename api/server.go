b4e459d7c55863d1dca7d9a33f3a1087fb04e0d6
xzhc <1838998336@qq.com>
2024-08-01 14:36:38 +0800

    add http api

---
+++ api/server.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/xzhc/simplebank/db/sqlc"
+)
+
+// Server servers HTTP requests for our banking service
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and set up routing
+func NewServer(sotre *db.Store) *Server {
+	server := &Server{store: sotre}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+	router.PUT("/accounts", server.updateAccount)
+	router.DELETE("/accounts/:id", server.deleteAccount)
+	server.router = router
+	return server
+}
+
+// Start runs the HTTP server on a specific address
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
